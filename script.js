64ce5bc98c0a6074fac51a6dc3348ee5da7ff17f
WilliamMcKenzie <williamqmckenzie@gmail.com>
2023-11-16 21:53:57 -0400

    bot creation

--- script.js
+++ script.js
@@ -6,90 +6,18 @@ var currentMoveIndex = 0
 
 const pieceVal = { 'p': 100, 'n': 280, 'b': 320, 'r': 479, 'q': 929, 'k': 60000, 'P' : -100, 'N' : -280, 'B' : -320, 'R' : -479, 'Q' : -929, 'K' : -60000}
 
-var pst_w = {
-  'p':[
-          [ 100, 100, 100, 100, 105, 100, 100,  100],
-          [  78,  83,  86,  73, 102,  82,  85,  90],
-          [   7,  29,  21,  44,  40,  31,  44,   7],
-          [ -17,  16,  -2,  15,  14,   0,  15, -13],
-          [ -26,   3,  10,   9,   6,   1,   0, -23],
-          [ -22,   9,   5, -11, -10,  -2,   3, -19],
-          [ -31,   8,  -7, -37, -36, -14,   3, -31],
-          [   0,   0,   0,   0,   0,   0,   0,   0]
-      ],
-  'n': [ 
-          [-66, -53, -75, -75, -10, -55, -58, -70],
-          [ -3,  -6, 100, -36,   4,  62,  -4, -14],
-          [ 10,  67,   1,  74,  73,  27,  62,  -2],
-          [ 24,  24,  45,  37,  33,  41,  25,  17],
-          [ -1,   5,  31,  21,  22,  35,   2,   0],
-          [-18,  10,  13,  22,  18,  15,  11, -14],
-          [-23, -15,   2,   0,   2,   0, -23, -20],
-          [-74, -23, -26, -24, -19, -35, -22, -69]
-      ],
-  'b': [ 
-          [-59, -78, -82, -76, -23,-107, -37, -50],
-          [-11,  20,  35, -42, -39,  31,   2, -22],
-          [ -9,  39, -32,  41,  52, -10,  28, -14],
-          [ 25,  17,  20,  34,  26,  25,  15,  10],
-          [ 13,  10,  17,  23,  17,  16,   0,   7],
-          [ 14,  25,  24,  15,   8,  25,  20,  15],
-          [ 19,  20,  11,   6,   7,   6,  20,  16],
-          [ -7,   2, -15, -12, -14, -15, -10, -10]
-      ],
-  'r': [  
-          [ 35,  29,  33,   4,  37,  33,  56,  50],
-          [ 55,  29,  56,  67,  55,  62,  34,  60],
-          [ 19,  35,  28,  33,  45,  27,  25,  15],
-          [  0,   5,  16,  13,  18,  -4,  -9,  -6],
-          [-28, -35, -16, -21, -13, -29, -46, -30],
-          [-42, -28, -42, -25, -25, -35, -26, -46],
-          [-53, -38, -31, -26, -29, -43, -44, -53],
-          [-30, -24, -18,   5,  -2, -18, -31, -32]
-      ],
-  'q': [   
-          [  6,   1,  -8,-104,  69,  24,  88,  26],
-          [ 14,  32,  60, -10,  20,  76,  57,  24],
-          [ -2,  43,  32,  60,  72,  63,  43,   2],
-          [  1, -16,  22,  17,  25,  20, -13,  -6],
-          [-14, -15,  -2,  -5,  -1, -10, -20, -22],
-          [-30,  -6, -13, -11, -16, -11, -16, -27],
-          [-36, -18,   0, -19, -15, -15, -21, -38],
-          [-39, -30, -31, -13, -31, -36, -34, -42]
-      ],
-  'k': [  
-          [  4,  54,  47, -99, -99,  60,  83, -62],
-          [-32,  10,  55,  56,  56,  55,  10,   3],
-          [-62,  12, -57,  44, -67,  28,  37, -31],
-          [-55,  50,  11,  -4, -19,  13,   0, -49],
-          [-55, -43, -52, -28, -51, -47,  -8, -50],
-          [-47, -42, -43, -79, -64, -32, -29, -32],
-          [ -4,   3, -14, -50, -57, -18,  13,   4],
-          [ 17,  30,  -3, -14,   6,  -1,  40,  18]
-    ]
-};
-
-var pst_b = {
-  'p': pst_w['p'].slice().reverse(),
-  'n': pst_w['n'].slice().reverse(),
-  'b': pst_w['b'].slice().reverse(),
-  'r': pst_w['r'].slice().reverse(),
-  'q': pst_w['q'].slice().reverse(),
-  'k': pst_w['k'].slice().reverse()
-}
-
 var positionMap = {
-  'p':[
+  'P':[
           [ 100, 100, 100, 100, 105, 100, 100,  100],
           [  78,  83,  86,  73, 102,  82,  85,  90],
           [   7,  29,  21,  44,  40,  31,  44,   7],
           [ -17,  16,  -2,  15,  14,   0,  15, -13],
-          [ -26,   3,  10,   9,   6,   1,   0, -23],
-          [ -22,   9,   5, -11, -10,  -2,   3, -19],
+          [ -26,   3,  10,   30,  30,   1,   0, -23],
+          [ -22,   9,   5, 20, 20,  -2,   3, -19],
           [ -31,   8,  -7, -37, -36, -14,   3, -31],
           [   0,   0,   0,   0,   0,   0,   0,   0]
       ],
-  'n': [ 
+  'N': [ 
           [-66, -53, -75, -75, -10, -55, -58, -70],
           [ -3,  -6, 100, -36,   4,  62,  -4, -14],
           [ 10,  67,   1,  74,  73,  27,  62,  -2],
@@ -99,7 +27,7 @@ var positionMap = {
           [-23, -15,   2,   0,   2,   0, -23, -20],
           [-74, -23, -26, -24, -19, -35, -22, -69]
       ],
-  'b': [ 
+  'B': [ 
           [-59, -78, -82, -76, -23,-107, -37, -50],
           [-11,  20,  35, -42, -39,  31,   2, -22],
           [ -9,  39, -32,  41,  52, -10,  28, -14],
@@ -109,7 +37,7 @@ var positionMap = {
           [ 19,  20,  11,   6,   7,   6,  20,  16],
           [ -7,   2, -15, -12, -14, -15, -10, -10]
       ],
-  'r': [  
+  'R': [  
           [ 35,  29,  33,   4,  37,  33,  56,  50],
           [ 55,  29,  56,  67,  55,  62,  34,  60],
           [ 19,  35,  28,  33,  45,  27,  25,  15],
@@ -119,7 +47,7 @@ var positionMap = {
           [-53, -38, -31, -26, -29, -43, -44, -53],
           [-30, -24, -18,   5,  -2, -18, -31, -32]
       ],
-  'q': [   
+  'Q': [   
           [  6,   1,  -8,-104,  69,  24,  88,  26],
           [ 14,  32,  60, -10,  20,  76,  57,  24],
           [ -2,  43,  32,  60,  72,  63,  43,   2],
@@ -129,7 +57,7 @@ var positionMap = {
           [-36, -18,   0, -19, -15, -15, -21, -38],
           [-39, -30, -31, -13, -31, -36, -34, -42]
       ],
-  'k': [  
+  'K': [  
           [  4,  54,  47, -99, -99,  60,  83, -62],
           [-32,  10,  55,  56,  56,  55,  10,   3],
           [-62,  12, -57,  44, -67,  28,  37, -31],
@@ -140,18 +68,18 @@ var positionMap = {
           [ 17,  30,  -3, -14,   6,  -1,  40,  18]
     ]
 };
-positionMap['P'] = positionMap['p'].slice().reverse()
-positionMap['N'] = positionMap['n'].slice().reverse()
-positionMap['B'] = positionMap['b'].slice().reverse()
-positionMap['R'] = positionMap['r'].slice().reverse()
-positionMap['Q'] = positionMap['q'].slice().reverse()
-positionMap['K'] = positionMap['k'].slice().reverse()
+positionMap['p'] = positionMap['P'].slice().reverse()
+positionMap['n'] = positionMap['N'].slice().reverse()
+positionMap['b'] = positionMap['B'].slice().reverse()
+positionMap['r'] = positionMap['R'].slice().reverse()
+positionMap['q'] = positionMap['Q'].slice().reverse()
+positionMap['k'] = positionMap['K'].slice().reverse()
 
 var balance = 0
 var selectedDepth = -1
 var demonMode = false
 
-function eval(gameToEval, side, fen){
+function evaluate(gameToEval, side, fen){
   var total = 0
   var layout = fen.split(" ")[0]
 
@@ -213,7 +141,7 @@ function depthSearchMed(depth, originalDepth, algo, a, b){
     
     else {
       var currentGame = algo.fen()
-      var rating = eval(algo, side, currentGame)
+      var rating = evaluate(algo, side, currentGame)
       if(yourTurn && rating < b){
         b = rating
       }
@@ -265,7 +193,7 @@ function depthSearch(depth, originalDepth, algo, totalEval, side, rootMove, curr
     if(depth+1 == originalDepth){
       //for if depth is only 1, just attack whenever possible
       if(originalDepth == 1){
-        var curEval = eval(algo, side, fen)
+        var curEval = evaluate(algo, side, fen)
         if(curEval > max[0]){
           max = [curEval, moves[i]]
         }
@@ -296,7 +224,7 @@ function depthSearch(depth, originalDepth, algo, totalEval, side, rootMove, curr
     }
     //at last iteration, evaluate min
     else {
-      var curEval = eval(algo, side, fen)
+      var curEval = evaluate(algo, side, fen)
       
       if(curEval < currentSafest && originalDepth % 2 == 0){
         return min
@@ -325,8 +253,7 @@ function moveAI () {
     optimalMove = possibleMoves[randomIdx]
   }
   else if(selectedDepth != 2 & selectedDepth != 4){
-    var depth = depthSearch(selectedDepth, selectedDepth, algo, eval(algo, game.turn(), game.fen()), game.turn(), '', 1000, -1000)
-    console.log(depth)
+    var depth = depthSearch(selectedDepth, selectedDepth, algo, evaluate(algo, game.turn(), game.fen()), game.turn(), '', 1000, -1000)
     optimalMove = depth[1]
   }
   else{
@@ -341,7 +268,7 @@ function moveAI () {
 
   // exit if the game is over
   if (game.game_over()){
-    document.getElementById('face_speechbox').innerHTML = 'GG lousy CHIGGER!'
+    document.getElementById('face_speechbox').innerHTML = 'Good game!'
     document.getElementById('restart').style.visibility = 'visible'
     return
   } 
@@ -360,7 +287,7 @@ function onDragStart (source, piece, position, orientation) {
   }
 }
 
-function onDrop (source, target) {
+async function onDrop (source, target) {
   // see if the move is legal
   var move = game.move({
     from: source,
@@ -370,23 +297,17 @@ function onDrop (source, target) {
 
   // illegal move
   if (move === null) return 'snapback'
+  moveHistory.push(game.fen())
 
-  return new Promise((resolve) => {
-    animateMove(move, () => {
-      // Update chessboard graphics
-      board.position(game.fen())
-      
-      // Resolve the Promise to trigger AI move generation
-      resolve();
-    });
-  });
+  await sleep(500)
+  moveAI()
+  board.position(game.fen())
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
-function onSnapEnd () {
-  moveAI()
-  moveHistory.push(game.fen())
+async function onSnapEnd () {
+  board.position(game.fen())
 }
 
 var config = {
@@ -429,20 +350,13 @@ function onFenEdit(){
   game = new Chess(fenEle.value)
   board.position(game.fen())
 }
-
-function undo(){
-  game = new Chess(moveHistory[moveHistory.length-2])
-  console.log(moveHistory[moveHistory.length-2])
-  moveHistory.pop()
-  board.position(game.fen())
-}
 function reset(){
   game = new Chess()
   board.position(game.fen())
 }
 function onMove(){
   var scoreEle = document.getElementById('score')
-  var score = eval(game, 'white', game.fen())/10
+  var score = evaluate(game, 'white', game.fen())/10
   scoreEle.innerHTML = ` White: ${score*-1} | Black: ${score*1}`
 }
 
@@ -475,48 +389,58 @@ const dialogueOptions = {
     "I've mastered the art of chess.",
     "You're in for a serious game.",
     "Do you even stand a chance?",
-    "I see your every move in advance."
+    "I see your every move in advance.",
+    "I cant let you get close!",
+    "Fucking amateur."
   ],
   ultimate: [
     "I am the chess grandmaster.",
     "Your fate is sealed from the start.",
     "This is more of a lesson than a game.",
-    "I play on a level beyond imagination."
+    "I play on a level beyond imagination.",
+    "I cant let you get close!",
+    "Fucking amateur."
   ]
 };
 
 var face = document.getElementById('face')
 var speech = document.getElementById('face_speechbox')
+var robotNameEle = document.getElementById('robot_name')
 function onBraindeadSelected(){
   face.src = 'https://api.dicebear.com/7.x/bottts/svg?seed=skinnyfat'
   speech.innerHTML = 'How does the knight move again?'
+  robotNameEle.innerHTML = 'POTLICK'
   selectedDepth = -1
 }
 function onEasySelected(){
   face.src = 'https://api.dicebear.com/7.x/bottts/svg?seed=meditti'
   speech.innerHTML = 'Lets have a good match!'
+  robotNameEle.innerHTML = 'CHESTER'
   selectedDepth = 1
-  console.log(selectedDepth)
 }
 function onMediumSelected(){
   face.src = 'https://api.dicebear.com/7.x/bottts/svg?seed=hamza'
   speech.innerHTML = 'Challenging me? How foolish.'
+  robotNameEle.innerHTML = 'BARLEY'
   selectedDepth = 2
 }
 function onHardSelected(){
   face.src = 'https://api.dicebear.com/7.x/bottts/svg?seed=pooper'
   speech.innerHTML = 'You cannot win!'
+  robotNameEle.innerHTML = 'SANDER'
   selectedDepth = 3
 }
 function onDemonSelected(){
   face.src = 'https://api.dicebear.com/7.x/bottts/svg?seed=earss'
   speech.innerHTML = 'It is time to taste my glory.'
+  robotNameEle.innerHTML = 'MASTER HOMO-YO'
   selectedDepth = 3
   demonMode = true
 }
 function onSSSelected(){
   face.src = 'https://api.dicebear.com/7.x/bottts/svg?seed=mwowww'
   speech.innerHTML = 'Beep boop, calculating your defeat...'
+  robotNameEle.innerHTML = 'VIAGRO III'
   selectedDepth = 4
   demonMode = false
 }
@@ -524,13 +448,29 @@ function onSSSelected(){
 function saySomething(){
   var mode 
 
-  if(selectedDepth == -1) mode = dialogueOptions.dummy
-  else if(selectedDepth == 1) mode = dialogueOptions.easy
-  else if(selectedDepth == 2) mode = dialogueOptions.medium
-  else if(selectedDepth == 3 && demonMode != true) mode = dialogueOptions.hard
-  else if(selectedDepth == 3) mode = dialogueOptions.elite
+  if(selectedDepth == -1){
+     mode = dialogueOptions.dummy
+     robotNameEle.innerHTML = 'POTLICK'
+  }
+  else if(selectedDepth == 1){
+     mode = dialogueOptions.easy
+     robotNameEle.innerHTML = 'CHESTER'
+  }
+  else if(selectedDepth == 2){ 
+    mode = dialogueOptions.medium
+    robotNameEle.innerHTML = 'BARLEY'
+  }
+  else if(selectedDepth == 3 && demonMode != true){ 
+    mode = dialogueOptions.hard
+    robotNameEle.innerHTML = 'SANDER'
+  }
+  else if(selectedDepth == 3){
+     mode = dialogueOptions.elite
+     robotNameEle.innerHTML = 'MASTER HOMO-YO'  
+  }
   else {
     mode = dialogueOptions.ultimate
+    robotNameEle.innerHTML = 'VIAGRO III'  
   }
 
   var randomIdx = Math.floor(Math.random() * mode.length)
@@ -541,6 +481,27 @@ function saySomething(){
 function reloadWindow(){
   window.location.reload() 
   document.getElementById('restart').style.visibility = 'hidden'
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function saveBot() {
+      const botCode = document.getElementById('codeInput').value;
+      var script = document.createElement('script');
+      script.innerHTML = botCode
+      document.head.appendChild(script);
+}
+
+function useBot(){
+  window.setTimeout(moveCustomBot(), 500)
+}
+
+function moveCustomBot(){
+  game.move(bot1(game.moves(), game))
+  board.position(game.fen())
+  window.setTimeout(moveCustomBot, 500)
 }
 
 // moveAI()
